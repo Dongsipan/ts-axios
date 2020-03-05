7c7e0eb113e57a19c3a6716973f46789983e8f24
Dongsipan <dongsipan@gmail.com>
2020-03-05 21:51:42 +0800

    获取响应数据

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise } from './types'
 import xhr from './xhr'
-import { buildURL } from './helpers/url' 
+import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
 import { processHeaders } from './helpers/headers'
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config)
 }
 
-function processConfig (config: AxiosRequestConfig): void {
+function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
   config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
-function transformUrl (config: AxiosRequestConfig): string {
-  const {url, params} = config
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
   return buildURL(url, params)
 }
 
-function transformRequestData (config: AxiosRequestConfig): any {
+function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
 }
 
-function transformHeaders (config: AxiosRequestConfig) {
+function transformHeaders(config: AxiosRequestConfig) {
   const { headers = {}, data } = config
 
   return processHeaders(headers, data)
