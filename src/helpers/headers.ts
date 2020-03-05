803b91db33d49430d23e25d5503d60fb147fcb22
Dongsipan <dongsipan@gmail.com>
2020-03-05 21:56:24 +0800

    处理响应 header #需求分析

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -1,6 +1,6 @@
 import { isPlainObject } from './util'
 
-function normalizeHeaderName (headers: any, normalizedName: string): void {
+function normalizeHeaderName(headers: any, normalizedName: string): void {
   if (!headers) return
   Object.keys(headers).forEach(name => {
     if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
@@ -10,7 +10,7 @@ function normalizeHeaderName (headers: any, normalizedName: string): void {
   })
 }
 
-export function processHeaders (headers: any, data: any): any {
+export function processHeaders(headers: any, data: any): any {
   normalizeHeaderName(headers, 'Content-Type')
 
   if (isPlainObject(data)) {
@@ -19,4 +19,25 @@ export function processHeaders (headers: any, data: any): any {
     }
   }
   return headers
+}
+
+export function parseHeaders(headers: string): any {
+  let parsed = Object.create(null)
+  if (!headers) {
+    return parsed
+  }
+
+  headers.split('\r\n').forEach(line => {
+    let [key, val] = line.split(':')
+    key = key.trim().toLowerCase()
+    if (!key) {
+      return
+    }
+    if (val) {
+      val = val.trim()
+    }
+    parsed[key] = val
+  })
+
+  return parsed
 }
