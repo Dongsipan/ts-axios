294a3a03bcc5adac4bacea3ad1f7ca4bfb4414e8
dongsipan <75098051@qq.com>
2020-03-06 15:28:44 +0800

    优化

--- examples/server.js
+++ examples/server.js
@@ -10,33 +10,9 @@ const compiler = webpack(WebpackConfig)
 
 const router = express.Router()
 
-router.get('/simple/get', function(req, res) {
-  res.json({
-    msg: `hello world`
-  })
-})
+registerSimpleRouter()
 
-router.get('/base/get', function(req, res) {
-  res.json(req.query)
-})
-
-
-router.post('/base/post', function(req, res) {
-  res.json(req.body)
-})
-
-router.post('/base/buffer', function(req, res) {
-  let msg = []
-  req.on('data', (chunk) => {
-    if (chunk) {
-      msg.push(chunk)
-    }
-  })
-  req.on('end', () => {
-    let buf = Buffer.concat(msg)
-    res.json(buf.toJSON())
-  })
-})
+registerBaseRouter()
 
 app.use(router)
 
@@ -59,3 +35,34 @@ const port = process.env.PORT || 8100
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
+
+function registerSimpleRouter () {
+  router.get('/simple/get', function(req, res) {
+    res.json({
+      msg: `hello world`
+    })
+  })
+}
+
+function registerBaseRouter () {
+  router.get('/base/get', function(req, res) {
+    res.json(req.query)
+  })
+
+  router.post('/base/post', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.post('/base/buffer', function(req, res) {
+    let msg = []
+    req.on('data', (chunk) => {
+      if (chunk) {
+        msg.push(chunk)
+      }
+    })
+    req.on('end', () => {
+      let buf = Buffer.concat(msg)
+      res.json(buf.toJSON())
+    })
+  })
+}
